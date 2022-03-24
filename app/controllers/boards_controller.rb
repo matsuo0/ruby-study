c8f03f18b9b082dcbea849b6bb68504255983646
matsuo0 <uno831@gmail.com>
2022-03-24 11:51:18 +0900

    ?

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,52 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
+
   def index
+    @boards = Board.all
   end
 
   def new
+    @board = Board.new
+    # byebug
+    # binding.pry
   end
+
+  def create
+    board = Board.create(board_params)
+    redirect_to board
+    # binding.pry
+  end
+
+  def show
+    # @board = Board.find(params[:id])    
+  end
+
+  def edit
+    # @board = Board.find(params[:id])
+  end
+
+  def update
+    # board = Board.find(params[:id])
+    board.update(board_params)
+
+    redirect_to board
+  end
+  
+  def destroy
+    # board = Board.find(params[:id])
+    board.delete
+
+    redirect_to boards_path
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
+
 end
